c607d4170a7f5f392524c32789e91c59e197fd7f
Rahul <vaish.rahul4810@gmail.com>
2020-07-02 12:26:04 +0530

    SpeakingBot

--- SpeakingBotPy/SpeakingBot-STT-TTS.py
+++ SpeakingBotPy/SpeakingBot-STT-TTS.py
@@ -19,9 +19,7 @@
 # pip3 install SpeechRecognition
 # python3 -m spacy download en
 # pip3 install pyaudio
-
-
-
+#=================================================
 import pyttsx3
 import configparser
 import speech_recognition as sr
@@ -29,66 +27,88 @@ from configparser import ConfigParser
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-bot = ChatBot('MOVI')
-trainer = ListTrainer(bot)
-
-# #### Loading and Training the ChatBot
-separator = "="
-with open('ChatbotTraining.properties') as f:
-    for line in f:
-        if separator in line:
-            name, value = line.split(separator, 1)
-            trainer.train([name, value])
-
-# #### Converting Speech To Text
-r = sr.Recognizer()
-
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)
-    print("Please say something")
-    audio = r.listen(source)
-    print("Recognizing Now .... ")
-    # Using sphinx API
-    #voiceheard = r.recognize_sphinx(audio);
-    
-    # Using Google API
-    voiceheard = r.recognize_google(audio);
-    try:
-        print("You have said: " + voiceheard)
-    except Exception as e:
-        print("Error :  " + str(e))
-
-# #### Returning answer [as text] from the question [as speech]
-speechResponse= bot.get_response(voiceheard)
+#Initializing ChatBot
+bot = ChatBot('MOVP')
 
 
-# #### Converting Text To Speech
-engine = pyttsx3.init() # object creation
-ttsconfig = configparser.ConfigParser()
-ttsconfig.read('TTS.properties')
+#Loading and Training the ChatBot
+def loadAndTrainChatBot():
+    trainer = ListTrainer(bot)
+    separator = "="
+    with open('ChatbotTraining.properties') as f:
+        for line in f:
+            if separator in line:
+                name, value = line.split(separator, 1)
+                trainer.train([name, value])
 
-""" RATE"""
-rate = engine.getProperty('rate')    # getting details of current speaking rate
-#print (rate)                        # printing current voice rate
-engine.setProperty('rate', ttsconfig.get("TTS", "rate"))     # setting up new voice rate
+
+                
+                
+#Converting Speech To Text
+def convertSpeechToText(): 
+    r = sr.Recognizer() 
+    audio = ''  
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("Please Speak...") 
+        audio = r.listen(source, phrase_time_limit = 5)  
+    try: 
+        text = r.recognize_google(audio, language ='en-US') 
+        print("You said: ", text) 
+        return text 
+    except: 
+        convertTextToSpeech("Time our or I counn't hear properly") 
+        return 0                
+                
+
+        
+
+#Drive Speech To Text
+def executeSpeechToText():
+    while(1): 
+        text = convertSpeechToText() 
+        if text == 0: 
+            continue
+        if "exit" in str(text) or "bye" in str(text): 
+            convertTextToSpeech("Thank you for using the Application") 
+            break
+        botresponse= bot.get_response(text)
+        convertTextToSpeech(botresponse) 
 
 
-"""VOLUME"""
-volume = engine.getProperty('volume')    # getting to know current volume level (min=0 and max=1)
-#print (volume)                          # printing current volume level
-engine.setProperty('volume', ttsconfig.get("TTS", "volume")) # setting up volume level  between 0 and 1
 
-"""VOICE"""
-voices = engine.getProperty('voices')       # getting details of current voice
-#engine.setProperty('voice', voices[0].id)  # changing index, changes voices. o for male
-engine.setProperty('voice', ttsconfig.get("TTS", "voices")) # changing index, changes voices. 1 for female
 
-engine.say(speechResponse)
-engine.runAndWait()
-engine.stop()
+#Converting Text To Speech
+def convertTextToSpeech(speechResponse):
+    engine = pyttsx3.init()
+    ttsconfig = configparser.ConfigParser()
+    ttsconfig.read('TTS.properties')
 
-# """Saving Voice to a file"""
-# On linux make sure that 'espeak' and 'ffmpeg' are installed
-# engine.save_to_file('Hello World', 'test.mp3')
-# engine.runAndWait()
+    """ RATE"""
+    rate = engine.getProperty('rate')    
+    engine.setProperty('rate', ttsconfig.get("TTS", "rate"))     # setting up new voice rate
+
+    """VOLUME"""
+    volume = engine.getProperty('volume')
+    engine.setProperty('volume', ttsconfig.get("TTS", "volume")) # setting up volume level  between 0 and 1
+
+    """VOICE"""
+    voices = engine.getProperty('voices')     
+    #engine.setProperty('voice', voices[0].id)  # voices[0].id for male
+    engine.setProperty('voice', voices[int(ttsconfig.get("TTS", "voices"))].id) # voices[1].id for female
+
+    engine.say(speechResponse)
+    engine.runAndWait()
+    engine.stop()
+
+
+
+
+#Driver
+def main():
+    loadAndTrainChatBot()
+    executeSpeechToText()
+
+if __name__ == "__main__":
+    main()    
 
